5d2c33fc3403651c419134e28422f00e9a3d7489
Dominic DeRosa <dderosa71@gmail.com>
2021-08-11 14:32:45 +0200

    Done.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// import React from "react"
+// import EyesOnMe from "./EyesOnMe.js"
+// import Keypad from "./Keypad.js"
+
+// function App(){
+//     return(
+//         <main>
+//         <EyesOnMe/>
+//         <Keypad/>
+//         </main>
+//     )
+// }
+
+// export default App
--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe(){
+    function handleLostFocus(){
+        console.log('Hey! Eyes on me!')
+    }
+    return(<button onFocus={()=>console.log('Good!')} onBlur={()=>console.log('Hey! Eyes on me!')}>Eyes on me</button>)
+}
+
+export default EyesOnMe
